5a27d5931913234b3dcff0be9c857fd07b91dd38
Timothy Carter <talic2002@gmail.com>
2022-05-03 10:49:09 -0400

    changed port config

--- index.js
+++ index.js
@@ -41,4 +41,6 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
   .then(m => console.log('db connected'))
   .catch(e => console.log(e))
 
-app.listen(process.env.PORT || 4000, () => console.log(`Listening on ${process.env.PORT || 4000}`))
+// app.listen(process.env.PORT || 4000, () => console.log(`Listening on ${process.env.PORT || 4000}`))
+app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
+
