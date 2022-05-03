827e856f9cbb9bf1c5c3b376042aec3856c6b74f
Timothy Carter <talic2002@gmail.com>
2022-05-03 16:16:10 -0400

    correction to mongoose connect

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('*', (req, res) => {
 })
 
 // database
-mongoose.connect(`${process.env.MONGO_URI}`, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`mongodb+srv://${MONGO_URI}admin:admin@cluster0.os38h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(m => console.log('db connected'))
   .catch(e => console.log(e))
 
