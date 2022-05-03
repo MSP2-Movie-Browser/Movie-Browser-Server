9e2a8e368854c5daf628fb1e7856344da07a9287
Timothy Carter <talic2002@gmail.com>
2022-05-03 16:11:02 -0400

    updated mongoose connection string

--- index.js
+++ index.js
@@ -40,10 +40,8 @@ app.get('*', (req, res) => {
 })
 
 // database
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`${process.env.MONGO_URI}`, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(m => console.log('db connected'))
   .catch(e => console.log(e))
 
 app.listen(process.env.PORT || 3005, () => console.log(`Listening on ${process.env.PORT || 3005}`))
-// app.listen(process.env.PORT || 3005, () => console.log(`Listening on ${process.env.PORT} \\`))
-
